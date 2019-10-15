4aaf89a8d0a84faa7d4899a7b7bc458601fd6c60
Biser Perchinkov <biser@bisko.be>
2019-10-15 18:35:51 +0300

    Remove yargs and add a simpler alternative

--- start.js
+++ start.js
@@ -1,48 +1,53 @@
-// TODO parse Yargs
 // TODO fetch show info
 // TODO fetch all pages from archive
 // TODO prepare all the episodes as json
-const request = require('request-promise-native');
-const cheerio = require('cheerio');
+const request = require( 'request-promise-native' );
+const cheerio = require( 'cheerio' );
+
+const ArgumentParser = require( 'argparse' ).ArgumentParser;
+
 const URLs = {
 	showInfo: 'http://archive.bnt.bg/bg/productions/about/{ID}/',
 };
 
-const parseShowInfo = ( argv ) => {
-	const id = argv.id;
-	const url = URLs.showInfo.replace(/\{ID\}/, id);
+const parseShowInfo = ( id ) => {
+	const url = URLs.showInfo.replace( /\{ID\}/, id );
 
-	return new Promise((resolve, reject) => {
-		request(url)
-			.then((html) => {
-				const $ = cheerio.load(html, { decodeEntities: false });
-				const title = $('.content h3').text();
-				const showInfo = $('.content .rich-text').html();
+	return new Promise( ( resolve, reject ) => {
+		request( url )
+			.then( ( html ) => {
+				const $ = cheerio.load( html, { decodeEntities: false } );
+				const title = $( '.content h3' ).text();
+				const showInfo = $( '.content .rich-text' ).html();
 
-				resolve({
+				resolve( {
 					showId: id,
 					showTitle: title,
 					showInfo: showInfo
-				});
+				} );
 
-			})
-			.catch(reject); // TODO better error handling
-	});
+			} )
+			.catch( reject ); // TODO better error handling
+	} );
 };
 
 
-require( 'yargs' )
-	.scriptName( 'bnt-archive-parser' )
-	.usage( '$0 <cmd> [Yargs]' )
-	.command( 'showinfo [id]', 'Fetch show info by ID', ( yargs ) => {
-		yargs.positional( 'id', {
-			type: 'number',
-			default: null,
-			describe: 'The show ID'
-		} );
-	}, (argv) => {
-		parseShowInfo(argv).then(console.log)
-	})
-	.help()
-	.argv;
+const parser = new ArgumentParser( {
+	version: '0.0.1',
+	addHelp: true,
+	description: 'Parses BNT\'s archive for show information and links to video files',
+} );
 
+
+parser.addArgument(
+	['-si', '--showInfo'],
+	{
+		help: 'Fetch information about a show'
+	}
+);
+
+const args = parser.parseArgs();
+
+if ( args.showInfo ) {
+	parseShowInfo( args.showInfo ).then( console.log );
+}
